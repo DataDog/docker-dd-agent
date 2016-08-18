88345efa06817608416d18f0e16a5c19bf8ec802
Massimiliano Pippi <massi@datadoghq.com>
2016-08-18 19:17:36 +0200

    added WARNING on event setting

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -92,6 +92,7 @@ if [[ $KUBERNETES ]]; then
     cp /opt/datadog-agent/agent/conf.d/kubernetes.yaml.example /opt/datadog-agent/agent/conf.d/kubernetes.yaml
 
     # enable event collector
+    # WARNING: to avoid duplicates, only one agent at a time across the entire cluster should have this feature enabled.
     if [[ $KUBERNETES_COLLECT_EVENTS ]]; then
         sed -i -e "s@# collect_events: false@collect_events: true@" /opt/datadog-agent/agent/conf.d/kubernetes.yaml
     fi
