80ee296df51e7d4979aa65dc460b175fb40bb05b
Hippolyte HENRY <hippolyte.henry@dadadoghq.com>
2017-02-21 11:55:57 -0500

    Fix error showing in supervisor logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ fi
 
 if [[ $LOGS_STDOUT == "yes" ]]; then
   sed -i -e "/^.*_logfile.*$/d" /etc/dd-agent/supervisor.conf
-  sed -i -e "/^.*\[program:.*\].*$/a stdout_logfile=\/dev\/stdout\nstdout_logfile_maxsize=0\nstderr_logfile=\/dev\/stderr\nstderr_logfile_maxsize=0" /etc/dd-agent/supervisor.conf
+  sed -i -e "/^.*\[program:.*\].*$/a stdout_logfile=\/dev\/stdout\nstdout_logfile_maxbytes=0\nstderr_logfile=\/dev\/stderr\nstderr_logfile_maxbytes=0" /etc/dd-agent/supervisor.conf
 fi
 
 if [[ $DD_URL ]]; then
