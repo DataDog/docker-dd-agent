45cf41b4491e6be07533d7472fa84a560dbc57d6
Aaditya Talwai <aaditya.21@gmail.com>
2017-05-16 11:22:19 -0400

    add DD_APM_ENABLED to dogstatsd entrypoint

--- dogstatsd/entrypoint.sh
+++ dogstatsd/entrypoint.sh
@@ -16,6 +16,12 @@ if [[ $DD_URL ]]; then
     sed -i -e 's@^.*dd_url:.*$@dd_url: '${DD_URL}'@' /etc/dd-agent/datadog.conf
 fi
 
+# ensure that the trace-agent doesn't run unless instructed to
+export DD_APM_ENABLED=false
+if [[ $DD_APM_ENABLED ]]; then
+  export DD_APM_ENABLED=${DD_APM_ENABLED}
+fi
+
 export PATH="/opt/datadog-agent/embedded/bin:/opt/datadog-agent/bin:$PATH"
 
 exec "$@"
