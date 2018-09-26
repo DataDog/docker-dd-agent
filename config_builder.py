97ae28712a340305ee2a64205cb008e897bd30dd
Vishal Shah <vishshah00@gmail.com>
2018-09-26 07:04:15 -0700

    add HISTOGRAM_AGGREGATES to configurable env_variables and config_builer.py (#315)

--- config_builder.py
+++ config_builder.py
@@ -69,6 +69,8 @@ class ConfBuilder(object):
         self.set_from_env_mapping('DD_URL', 'dd_url')
         self.set_from_env_mapping('STATSD_METRIC_NAMESPACE', 'statsd_metric_namespace')
         self.set_from_env_mapping('USE_DOGSTATSD', 'use_dogstatsd')
+        # Histogram Aggregates and Histogram Percentile configuration
+        self.set_from_env_mapping('DD_HISTOGRAM_AGGREGATES', 'histogram_aggregates')
         self.set_from_env_mapping('DD_HISTOGRAM_PERCENTILES', 'histogram_percentiles')
         ##### Proxy config #####
         self.set_from_env_mapping('PROXY_HOST', 'proxy_host')
